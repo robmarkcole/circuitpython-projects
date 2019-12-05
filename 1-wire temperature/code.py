ffb4d3875fc8225624741332ade5e10e1e20dfa9
Robin Cole <robmarkcole@gmail.com>
2019-12-05 07:18:59 +0000

    black format

--- 1-wire temperature/code.py
+++ 1-wire temperature/code.py
@@ -9,7 +9,6 @@ import adafruit_esp32spi.adafruit_esp32spi_socket as socket
 from adafruit_onewire.bus import OneWireBus
 from adafruit_ds18x20 import DS18X20
 from adafruit_minimqtt import MQTT
-import adafruit_dotstar
 from secrets import secrets
 from config import config
 
@@ -31,32 +30,19 @@ spi = busio.SPI(board.SCK, board.MOSI, board.MISO)
 esp = adafruit_esp32spi.ESP_SPIcontrol(spi, esp32_cs, esp32_ready, esp32_reset)
 wifi = adafruit_esp32spi_wifimanager.ESPSPI_WiFiManager(esp, secrets)
 
+
 def connect(client, userdata, flags, rc):
-    print("Connected to MQTT Broker!")
+    print("Connected to MQTT Broker {}".format(secrets["mqtt-broker"]))
     print("Flags: {0}\n RC: {1}".format(flags, rc))
 
-def subscribe(client, userdata, topic, granted_qos):
-    # This method is called when the client subscribes to a new feed.
-    print("Subscribed to {0} with QOS level {1}".format(topic, granted_qos))
-
-def publish(client, userdata, topic, pid):
-#    print("Published to {0} with PID {1}".format(topic, pid))
-    return
 
 wifi.connect()
 print("Connected to wifi with IP {}".format(wifi.ip_address()))
 
-client = MQTT(
-    socket,
-    broker=secrets['mqtt-broker'],
-    port=1883,
-    network_manager=wifi,
-)
+client = MQTT(socket, broker=secrets["mqtt-broker"], port=1883, network_manager=wifi)
 
 # Connect callback handlers to client
 client.on_connect = connect
-client.on_subscribe = subscribe
-client.on_publish = publish
 client.connect()
 
 while True:
@@ -64,11 +50,11 @@ while True:
         temperature = ds18.temperature
         print((ds18.temperature,))
         led[0] = GREEN
-        client.publish(config['mqtt_topic'], temperature)
+        client.publish(config["mqtt_topic"], temperature)
         time.sleep(0.1)
         led[0] = RED
-        time.sleep(config['sleep_time'])
+        time.sleep(config["sleep_time"])
     except Exception as err:
-        print('An error occured: {}'.format(err))
+        print("An error occured: {}".format(err))
         wifi.reset()
         wifi.connect()
--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # circuitpython-projects
 circuitpython projects for demo/education. Requirements are imported manually from https://github.com/adafruit/Adafruit_CircuitPython_Bundle (download the zipped latest release).
 
-I recommend using [mu](https://github.com/mu-editor/mu) editor as it is ideal for fast iteration of the code. I normally would use vscode, and you can, but you will spend a lot of time reconnecting to the device with `screen` which really slows the development process.
+I recommend using [mu](https://github.com/mu-editor/mu) editor ([mu docs](https://mu.readthedocs.io/en/latest/)) as it is ideal for fast iteration of the code. I normally would use vscode, and you can, but you will spend a lot of time reconnecting to the device with `screen` which really slows the development process.
 
 ## Photoresistor
 A photoresistor is used to detect light/darkness, and toggle an LED colour. The signal from the photoresistor is plotted in real time by Mu.
